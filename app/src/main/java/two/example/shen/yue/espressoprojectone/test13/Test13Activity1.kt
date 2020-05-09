794c11e46ede2bcdca4c330b13a858e906761556
piimlt <zhuolei.jiang@softlinker.com>
2020-05-09 17:20:35 +0800

    update

---
+++ app/src/main/java/two/example/shen/yue/espressoprojectone/test13/Test13Activity1.kt
@@ -0,0 +1,21 @@
+package two.example.shen.yue.espressoprojectone.test13
+
+import android.os.Bundle
+import two.example.shen.yue.espressoprojectone.BaseActivity
+import two.example.shen.yue.espressoprojectone.R
+
+/**
+ * Author: Queen
+ * Date: 2020/4/20 2:21 PM
+ * Describe: TODO
+ */
+class Test13Activity1 : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test13_1)
+
+    }
+
+}
